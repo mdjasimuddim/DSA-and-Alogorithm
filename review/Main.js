59fa6400bf007210570cc7caab41d76b8711f080
mdjasimuddim <86920541+mdjasimuddim@users.noreply.github.com>
2022-12-11 22:35:16 +0600

    Delete Main.js

--- review/Main.js
+++
@@ -1,7 +0,0 @@
-console.log(1+1);
-console.log('Bangladesh is a small country in south asia');
-
-function greet(name, InitialMessage){
-    return InitialMessage + ' ' + name;
-}
-console.log(greet('Md Jasim uddim', 'Welcome'));
