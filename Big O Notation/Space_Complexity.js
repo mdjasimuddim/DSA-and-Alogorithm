9489f676d4634e890282b86ca194adf3bee4d7d9
mdjasimuddim <86920541+mdjasimuddim@users.noreply.github.com>
2022-12-15 00:56:40 +0600

    Add files via upload

---
+++ Big O Notation/Space_Complexity.js
@@ -0,0 +1,42 @@
+//We can use Big O notation to analyze space complexity.How much additional memory do we need to allocate
+// in order to run the code in our algorithm ?
+
+//Summarized from for space Complexity
+// Most primitives (Boolean, Numbers, undefined ,null ) are constant space 
+//Strings require O(n) space  (where n is the string length)
+//Reference types are generally O(n), where n is the length (for arrays) or the number of keys(for Objects)
+
+//Complexity is O(1)
+function sum(arr)
+{
+    let total = 0;
+    for(let i = 0; i< arr.length; i++){
+        total += arr[i];
+    }
+    return total;
+}
+console.log(sum([1,2,3,4,5]))
+
+
+function double(arr)
+{
+    let newArr = [];
+    for(let i = 0; i< arr.length; i++)
+    {
+        newArr.push(2*arr[i]);
+    }
+    return newArr;
+}
+console.log(double([1,2,3,4,5]))
+
+//To analyze the performance of an algorithm we use Big O Notation
+//Big O Notation can give us a high level understanding of time or space complexity of an algorithm
+//Time or Space complexity depends  only on algorithm , not the hardware used to run algorithm
+
+const num = 100; //O(1)
+const str = 'Bangladesh'; // O(n)
+const arr = [1,2,3,4,5];    //O(n)
+const obj = {
+    Name : 'jasim',
+    Roll: 1703035     //O(n)
+}
