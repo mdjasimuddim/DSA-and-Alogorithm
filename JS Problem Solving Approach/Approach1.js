e27eca023424c2b677959e85e2197341fbf7672b
mdjasimuddim <86920541+mdjasimuddim@users.noreply.github.com>
2022-12-16 17:02:53 +0600

    Add files via upload

---
+++ JS Problem Solving Approach/Approach1.js
@@ -0,0 +1,31 @@
+// 1.Understanding the problem 
+//     i).restate the problem in own words
+//     ii).what are the input and outputs
+//     iii).is there enough information to solve the problem 
+//     iv).Thinking of labelling data (don't use random label like i,j,k,a,b)
+//     v).checking the example with invalid, empty input
+
+// 2. Note own the details like Input, output,outcome
+// 3. Start with the naive/brute force approach.First thinks that come to mind that you're able to think well
+//and critically
+
+//Write a function that takes two numbers and return the result
+//Addition of two numbers
+//input 10 14 output -> 24
+//input 12 34 output -> 46
+
+function sum(num1, num2){
+    return num1+num2;
+}
+console.log(sum(10,14));
+console.log('md jasim uddim');
+
+// 4. Tell them why this approach is not best (O^2 or higher or not readable etc...)
+// 5. Walk through your approach, comment things and see where you may be able to break things.
+// 6. Before you start coding, walk through your code and write down the steps you are going to follow
+// 7. Breakdown the steps and commenting is the ultimate key, Rundown of thinking process, steps, Breaking
+// 8. start coding(simplify as necessary)
+// 9. can you write it better in therms of readability, time, space complexity ?
+// 10. If your interviewer is happy with the solution, the interviewer usually ends here. 
+// It is also Common that that the interviewer asks you extension questions.
+
