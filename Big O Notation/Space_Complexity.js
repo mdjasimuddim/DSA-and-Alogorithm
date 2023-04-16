ff8050973b6311fbf5aa5eee700b1e28c38a394d
Md Jasim Uddim <86920541+mdjasimuddim@users.noreply.github.com>
2023-04-16 17:00:34 +0600

    Update Space_Complexity.js
    updated space complexity

--- Big O Notation/Space_Complexity.js
+++ Big O Notation/Space_Complexity.js
@@ -9,7 +9,7 @@
 //Complexity is O(1)
 function sum(arr)
 {
-    let total = 0;
+    let total = 0; //primitives are always constant
     for(let i = 0; i< arr.length; i++){
         total += arr[i];
     }
@@ -20,8 +20,9 @@ console.log(sum([1,2,3,4,5]))
 
 function double(arr)
 {
-    let newArr = [];
-    for(let i = 0; i< arr.length; i++)
+    let newArr = [];         // space complexity for reference type like array are O(n)
+    for(let i = 0; i< arr.length; i++) //for let i = 1 is constant 
+        //O(n+1) = O(n)
     {
         newArr.push(2*arr[i]);
     }
